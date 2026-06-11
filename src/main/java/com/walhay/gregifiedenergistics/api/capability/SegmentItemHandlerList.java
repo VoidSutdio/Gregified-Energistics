cd3dfb9cef2383737402c4bb752b03a7c933f594
WALHAY <walhayggwp@gmail.com>
2026-06-11 18:28:39 +0300

    Add segment list handler bundled with unit testing

---
+++ src/main/java/com/walhay/gregifiedenergistics/api/capability/SegmentItemHandlerList.java
@@ -0,0 +1,130 @@
+package com.walhay.gregifiedenergistics.api.capability;
+
+import com.github.bsideup.jabel.Desugar;
+import gregtech.api.capability.impl.ItemHandlerList;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.ints.IntList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.items.IItemHandlerModifiable;
+
+/**
+ * SegmentItemHandlerList
+ *
+ * <p>Main idea of that class is to provide mutability of handler list unlike immutable {@link ItemHandlerList} which
+ * caches handlers on construction.
+ */
+public class SegmentItemHandlerList implements IItemHandlerModifiable {
+
+	private final LinkedList<IItemHandlerModifiable> handlers = new LinkedList<>();
+	private final IntList prefix = new IntArrayList();
+
+	public SegmentItemHandlerList(IItemHandlerModifiable... handlers) {
+		for (IItemHandlerModifiable handler : handlers) {
+			addHandler(handler);
+		}
+	}
+
+	public SegmentItemHandlerList(Collection<IItemHandlerModifiable> handlers) {
+		handlers.forEach(this::addHandler);
+	}
+
+	private static int upperBound(IntList prefix, int x) {
+		int lo = 0, hi = prefix.size();
+		while (lo < hi) {
+			int mid = (lo + hi) >>> 1;
+			if (prefix.get(mid) <= x) {
+				lo = mid + 1;
+			} else {
+				hi = mid;
+			}
+		}
+		return lo;
+	}
+
+	public void addHandler(IItemHandlerModifiable handler) {
+		handlers.add(handler);
+
+		if (prefix.size() > 0) {
+			prefix.add(prefix.get(prefix.size() - 1) + handler.getSlots());
+		} else {
+			prefix.add(handler.getSlots());
+		}
+	}
+
+	public void onHandlerChange(IItemHandlerModifiable handler) {
+		int index = handlers.indexOf(handler);
+
+		int oldSize = prefix.get(index) - (index == 0 ? 0 : prefix.get(index - 1));
+
+		int diff = handler.getSlots() - oldSize;
+
+		for (int i = index; i < prefix.size(); ++i) {
+			prefix.set(i, prefix.get(i) + diff);
+		}
+
+		if (handler.getSlots() == 0) {
+			handlers.remove(index);
+			prefix.remove(index);
+		}
+	}
+
+	public List<IItemHandlerModifiable> getHandlers() {
+		return handlers;
+	}
+
+	protected HandlerEntry getHandlerByGlobalIndex(int index) {
+		int handlerIndex = upperBound(prefix, index);
+
+		return new HandlerEntry(handlers.get(handlerIndex), handlerIndex == 0 ? 0 : prefix.get(handlerIndex - 1));
+	}
+
+	@Override
+	public int getSlots() {
+		return prefix.get(prefix.size() - 1);
+	}
+
+	@Override
+	public ItemStack getStackInSlot(int slot) {
+		HandlerEntry entry = getHandlerByGlobalIndex(slot);
+
+		return entry.handler == null ? null : entry.handler.getStackInSlot(slot - entry.index);
+	}
+
+	@Override
+	public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
+		HandlerEntry entry = getHandlerByGlobalIndex(slot);
+
+		return entry.handler == null ? stack : entry.handler.insertItem(slot - entry.index, stack, simulate);
+	}
+
+	@Override
+	public ItemStack extractItem(int slot, int amount, boolean simulate) {
+		HandlerEntry entry = getHandlerByGlobalIndex(slot);
+
+		return entry.handler == null
+				? ItemStack.EMPTY
+				: entry.handler.extractItem(slot - entry.index, amount, simulate);
+	}
+
+	@Override
+	public int getSlotLimit(int slot) {
+		HandlerEntry entry = getHandlerByGlobalIndex(slot);
+
+		return entry.handler == null ? 0 : entry.handler.getSlotLimit(slot - entry.index);
+	}
+
+	@Override
+	public void setStackInSlot(int slot, ItemStack stack) {
+		HandlerEntry entry = getHandlerByGlobalIndex(slot);
+
+		if (entry.handler != null) {
+			entry.handler.setStackInSlot(slot - entry.index, stack);
+		}
+	}
+
+	@Desugar
+	public record HandlerEntry(IItemHandlerModifiable handler, int index) {}
+}
---
+++ src/test/java/com/walhay/gregifiedenergistics/api/SegmentItemHandlerListTest.java
@@ -0,0 +1,138 @@
+package com.walhay.gregifiedenergistics.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.walhay.gregifiedenergistics.api.capability.SegmentItemHandlerList;
+import java.util.LinkedList;
+import java.util.Random;
+import net.minecraft.init.Bootstrap;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import net.minecraftforge.items.IItemHandlerModifiable;
+import net.minecraftforge.items.ItemStackHandler;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SegmentItemHandlerListTest {
+
+	private SegmentItemHandlerList handlerList;
+	private LinkedList<ItemStackHandler> handlers;
+	private static Random random = new Random(42);
+
+	@BeforeAll
+	public static void bootstrap() {
+		Bootstrap.register();
+	}
+
+	@BeforeEach
+	public void prepareTest() {
+		this.handlers = new LinkedList<>();
+
+		ItemStackHandler h1 = new ItemStackHandler(5);
+		ItemStackHandler h2 = new ItemStackHandler(9);
+		ItemStackHandler h3 = new ItemStackHandler(63);
+
+		this.handlers.add(h1);
+		this.handlers.add(h2);
+		this.handlers.add(h3);
+
+		fillWithRandomItems(h1, h2, h3);
+
+		this.handlerList = new SegmentItemHandlerList(h1, h2, h3);
+	}
+
+	private void fillWithRandomItems(IItemHandlerModifiable... handlers) {
+		Item[] items = ForgeRegistries.ITEMS.getValuesCollection().toArray(new Item[0]);
+
+		for (IItemHandlerModifiable handler : handlers) {
+			for (int i = 0; i < handler.getSlots(); ++i) {
+				Item randomItem = items[random.nextInt(items.length)];
+				int randomSize = 1 + random.nextInt(handler.getSlotLimit(i));
+
+				handler.setStackInSlot(i, new ItemStack(randomItem, randomSize));
+			}
+		}
+	}
+
+	private void checkContents() {
+		int totalSlots = this.handlers.stream()
+				.mapToInt(IItemHandlerModifiable::getSlots)
+				.sum();
+
+		assertEquals(totalSlots, this.handlerList.getSlots());
+
+		int currentIndex = 0;
+		int currentHandler = 0;
+		for (int i = 0; i < this.handlerList.getSlots(); ++i) {
+			if (currentIndex >= this.handlers.get(currentHandler).getSlots()) {
+				currentIndex = 0;
+				currentHandler++;
+			}
+
+			ItemStack expected = this.handlers.get(currentHandler).getStackInSlot(currentIndex++);
+
+			ItemStack actual = this.handlerList.getStackInSlot(i);
+
+			assertEquals(expected, actual);
+		}
+	}
+
+	private void performHandlerResize(int handlerIndex, int diff) {
+		ItemStackHandler changeHandler = this.handlers.get(handlerIndex);
+
+		int currentSize = changeHandler.getSlots();
+
+		changeHandler.setSize(Math.max(1, currentSize + diff));
+		this.handlerList.onHandlerChange(changeHandler);
+
+		checkContents();
+	}
+
+	@Test
+	public void handlerStaticTest() {
+		checkContents();
+	}
+
+	@Test
+	public void handlerDynamicTest() {
+		ItemStackHandler handler = new ItemStackHandler(16);
+
+		fillWithRandomItems(handler);
+
+		this.handlers.add(handler);
+		this.handlerList.addHandler(handler);
+
+		checkContents();
+	}
+
+	@Test
+	public void startHandlerResizeTest() {
+		performHandlerResize(0, 10);
+		performHandlerResize(0, -10);
+		performHandlerResize(0, 15);
+		performHandlerResize(0, 25);
+		performHandlerResize(0, -40);
+	}
+
+	@Test
+	public void midHandlerResizeTest() {
+		performHandlerResize(2, 10);
+		performHandlerResize(2, -10);
+		performHandlerResize(2, 15);
+		performHandlerResize(2, 25);
+		performHandlerResize(2, -40);
+	}
+
+	@Test
+	public void randomHandlerResizeTest() {
+		performHandlerResize(1, 10);
+		performHandlerResize(2, -3);
+		performHandlerResize(2, 15);
+		performHandlerResize(2, 23);
+		performHandlerResize(0, 40);
+		performHandlerResize(0, -17);
+		performHandlerResize(1, -40);
+	}
+}
